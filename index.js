d4ac366b930a76bbf852a9b0d5e58e9043889de5
gowdasumathi <127737255+gowdasumathi@users.noreply.github.com>
2023-03-30 21:29:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,149 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDoFji8gZ6nMmB2RJ6PlAHNyTZBE0SK84Q",
+    authDomain: "trial1-b7fda.firebaseapp.com",
+    databaseURL: "https://trial1-b7fda-default-rtdb.firebaseio.com/",
+    projectId: "trial1-b7fda",
+    storageBucket: "trial1-b7fda.appspot.com",
+    messagingSenderId: "300561345722",
+    appId: "1:300561345722:web:957ae5623895c24e2b6683",
+    measurementId: "G-HGDC7TE3TK"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  // Initialize variables
+  const auth = firebase.auth()
+  const database = firebase.database()
+  
+  // Set up our register function
+  function register () {
+    // Get all our input fields
+    email = document.getElementById('email').value
+    password = document.getElementById('password').value
+    full_name = document.getElementById('full_name').value
+    number = document.getElementById('number').value
+    ful_name = document.getElementById('ful_name').value
+  
+    // Validate input fields
+    if (validate_email(email) == false || validate_password(password) == false) {
+      alert('Email or Password is Outta Line!!')
+      return
+      // Don't continue running the code
+    }
+    if (validate_field(full_name) == false || validate_field(number) == false || validate_field(ful_name) == false) {
+      alert('One or More Extra Fields is Outta Line!!')
+      return
+    }
+   
+    // Move on with Auth
+    auth.createUserWithEmailAndPassword(email, password)
+    .then(function() {
+      // Declare user variable
+      var user = auth.currentUser
+  
+      // Add this user to Firebase Database
+      var database_ref = database.ref()
+  
+      // Create User data
+      var user_data = {
+        email : email,
+        full_name : full_name,
+        number : number,
+        last_login : Date.now()
+      }
+  
+      // Push to Firebase Database
+      database_ref.child('users/' + user.uid).set(user_data)
+  
+      // DOne
+      alert('User Created!!')
+    })
+    .catch(function(error) {
+      // Firebase will use this to alert of its errors
+      var error_code = error.code
+      var error_message = error.message
+  
+      alert(error_message)
+    })
+  }
+  
+  // Set up our login function
+  function login () {
+    // Get all our input fields
+    email = document.getElementById('email').value
+    password = document.getElementById('password').value
+  
+    // Validate input fields
+    if (validate_email(email) == false || validate_password(password) == false) {
+      alert('Enter the correct Password or Email')
+      return
+      // Don't continue running the code
+    }
+    if (validate_email(email) == true || validate_password(password) == true) {
+        alert('SUCCESSFUL LOGIN')
+            window.location.href="index.html";  
+          }
+
+    auth.signInWithEmailAndPassword(email, password)
+    .then(function() {
+      // Declare user variable
+      var user = auth.currentUser
+  
+      // Add this user to Firebase Database
+      var database_ref = database.ref()
+  
+      // Create User data
+      var user_data = {
+        last_login : Date.now()
+      }
+  
+      // Push to Firebase Database
+      database_ref.child('users/' + user.uid).update(user_data)
+
+  
+      // DOne
+      alert('User Logged In!!')
+  
+    })
+    
+    .catch(function(error) {
+      // Firebase will use this to alert of its errors
+      var error_code = error.code
+      var error_message = error.message
+  
+      alert(error_message)
+    })
+  }
+  // Validate Functions
+  function validate_email(email) {
+    expression = /^[^@]+@\w+(\.\w+)+\w$/
+    if (expression.test(email) == true) {
+      // Email is good
+      return true
+    } else {
+      // Email is not good
+      return false
+    }
+  }
+  
+  function validate_password(password) {
+    // Firebase only accepts lengths greater than 6
+    if (password < 6) {
+      return false
+    } else {
+      return true
+    }
+  }
+  
+  function validate_field(field) {
+    if (field == null) {
+      return false
+    }
+  
+    if (field.length <= 0) {
+      return false
+    } else {
+      return true
+    }
+  }
+  
